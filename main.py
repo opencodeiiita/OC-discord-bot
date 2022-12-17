9658d7d2133daf4533cd3b6f9d3af0018ec88f09
Pranav TIwari <pranavt150@gmail.com>
2022-12-18 04:37:19 +0530

    Added GammaMicrowave's personal command

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from discord.ext import commands 
+from discord.ext import commands
 import discord
 from server import start
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix = '-', intents = intents)
+bot = commands.Bot(command_prefix='-', intents=intents)
 
-@bot.event 
+
+@bot.event
 async def on_ready():
-  print('ready')
-  
+    print('ready')
+
+
 @bot.command()
 async def hi(ctx):
-  await ctx.send("Hello!!")
+    await ctx.send("Hello!!")
+
+# Discord: Gamma Microwave#4389 GitHub:GammaMicrowave
+
+
+@bot.command()
+async def GammaMicrowave(ctx):
+    embed = discord.Embed()
+    embed.set_image(
+        url="https://media.giphy.com/media/EtB1yylKGGAUg/giphy.gif")
+
+    await ctx.send(embed=embed)
+
 
 start()
 
-token = ""  #token will be provided with the every claimed issue
+token = ""
 bot.run(token)

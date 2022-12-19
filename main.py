a78b13fbc7eafe1921a4cabe62c6397f8bca5ee0
Rishi <rishichaturvedi0012@gmail.com>
2022-12-19 23:18:46 +0530

    adding a command for my username

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from discord.ext import commands 
 import discord
 from server import start
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,12 +15,19 @@ bot = commands.Bot(command_prefix = '-', intents = intents)
 @bot.event 
 async def on_ready():
   print('ready')
-  
+
 @bot.command()
 async def hi(ctx):
   await ctx.send("Hello!!")
 
+# Discord & Github Name: frikinomad
+@bot.command()
+async def frikinomad(ctx):
+  await ctx.send("Hello, I am frikinomad, I like to code and travel")
+
+
 start()
 
-token = ""  #token will be provided with the every claimed issue
+print(os.environ.get('TOKEN'))
+token = os.environ.get('TOKEN')  #token will be provided with the every claimed issue
 bot.run(token)
